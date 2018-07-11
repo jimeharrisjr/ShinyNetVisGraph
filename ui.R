d0e8806f7ec120094a18ab69b1bc410d6e7d7112
Jim Harris <jim@eunomicnetworks.com>
2018-07-10 23:46:10 -0400

    shinydashboard

--- ui.R
+++ ui.R
@@ -1,17 +1,16 @@
-
-
 library(shiny)
+library(shinydashboard)
 library(visNetwork)
 
-shinyUI(fluidPage(
+## ui.R ##
+library(shinydashboard)
 
-  # Application title
-  titlePanel("Network Visualization"),
-  mainPanel(br(),br(),br(),actionButton("gogobutt","Go"),
-            actionButton("stopbutt","Stop"),
-            actionButton("resetbutt","Reset"),
-            visNetworkOutput("network", height = '900px'), width = "90%", height = "100%"
-  )
-  # Sidebar with a slider input for number of bins
-  
-))
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(titlePanel("Network Visualization"),box(actionButton("gogobutt","Go"),
+                    actionButton("stopbutt","Stop"),
+                    actionButton("resetbutt","Reset"),
+                    visNetworkOutput("network", height = '900px'), width = "90%", height = "100%"
+  ))
+)
